23c48f3331e05a25d3823551aed18fbc1910567c
Arafa Dev <arafa.dev@gmail.com>
2024-07-30 23:16:57 +0300

    templete frontend 2

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use view;
 use Illuminate\Http\Request;
 
 class MessengerController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('messenger.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessengerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,7 +38,7 @@ Route::group(
     ],
     function () {
 
-        
+        Route::get('messenger', [MessengerController::class, 'index'])->name('home');
 
 
     }
